478737805cb2e4fbae27d4148498f7abeb27b4bb
Humza Waseem <humzawaseem95@gmail.com>
2017-05-29 02:41:57 +0500

    Added DOB option in register
    Added DOB option in register

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,6 +1,12 @@
 @extends ('user.masterlayout')
 
 
+@section('custom-css')
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+    @endsection
+
 @section ('content')
 
     <div id="page" class="page">
@@ -179,3 +185,24 @@
     </div>
 
     @endsection
+
+
+@section ('custom-script')
+
+    <script>
+        $(document).ready(function(){
+            var date_input=$('input[name="date"]'); //our date input has the name "date"
+            var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+            var options={
+                format: 'mm/dd/yyyy',
+                container: container,
+                todayHighlight: true,
+                autoclose: true,
+            };
+            date_input.datepicker(options);
+        })
+    </script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+
+
+@endsection
--- resources/views/user/partials/modal.blade.php
+++ resources/views/user/partials/modal.blade.php
@@ -34,10 +34,17 @@
                 <div class="box">
                     <div class="content registerBox" style="display:none;">
                         <div class="form">
+
                             <form method="post" html="{:multipart=>true}" data-remote="true" action="/register" accept-charset="UTF-8">
                                 <input id="email" class="form-control" type="text" placeholder="Email" name="email">
                                 <input id="password" class="form-control" type="password" placeholder="Password" name="password">
+
                                 <input id="password_confirmation" class="form-control" type="password" placeholder="Repeat Password" name="password_confirmation">
+
+
+                                    <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
+                              
+
                                 <input class="btn btn-default btn-register" type="submit" value="Create account" name="commit">
                             </form>
                         </div>
